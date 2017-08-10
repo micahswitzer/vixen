69e7e180d26c16dea851310890ab9682f215bff1
Micah Switzer <micah.switzer.dev@outlook.com>
2017-08-10 16:01:43 -0500

    Added CoolerMaster module

---
+++ Modules/Output/CoolerMaster/DataPolicyFactory.cs
@@ -0,0 +1,12 @@
+﻿using Vixen.Sys;
+
+namespace VixenModules.Output.CoolerMaster
+{
+    internal class DataPolicyFactory : IDataPolicyFactory
+    {
+        public IDataPolicy CreateDataPolicy()
+        {
+            return new DataPolicy();
+        }
+    }
+}
